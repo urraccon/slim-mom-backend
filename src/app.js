bbdd37fc040c547eab89b772709cd6d91bcc6e55
urraccon <133591634+urraccon@users.noreply.github.com>
2025-03-21 20:29:53 +0200

    updated cors configuration

--- src/app.js
+++ src/app.js
@@ -1,24 +1,24 @@
-import express from 'express';
-import morgan from 'morgan';
-import helmet from 'helmet';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import cookieParser from 'cookie-parser';
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cors from "cors";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
-import middlewares from './middlewares.js';
-import api from './api/index.js';
+import middlewares from "./middlewares.js";
+import api from "./api/index.js";
 
 const app = express();
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(helmet());
-app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
+app.use(cors({ credentials: true, origin: "http://localhost:5173" }));
 app.use(express.json());
 app.use(cookieParser());
 
-app.use('/api', api);
+app.use("/api", api);
 
 app.use(middlewares.notFound);
 app.use(middlewares.errorHandler);
